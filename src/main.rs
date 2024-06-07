6db8646e8fdcb04c491bb8360b064de1fad2fb80
German <germangolozubow@gmail.com>
2024-06-07 13:01:40 +0300

    Added argument parsing

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,31 @@
+use clap::Parser;
+use std::net;
+
 fn main() {
-    println!("Hello, world!");
+    let args: Args = clap::Parser::parse();
+    eprintln!(
+        "Starting the server.\n\
+        Current configuration:\n\
+        Addr: {}\n\
+        Root folder {}\n\
+        Threads number: {}",
+        args.socket_addr_v4,
+        args.root_folder_path,
+        args.threads_number
+    );
+}
+
+/// Simple multithreaded web server
+#[derive(Parser, Debug)]
+struct Args {
+    /// IP address and port that the server is listening on. It must be in the format IP:PORT.
+    /// 127.0.0.1:7878 for example.
+    #[arg(id = "addr", short, long, default_value = "127.0.0.1:7878")]
+    socket_addr_v4: String,
+    /// Path to the folder that contains the site files.
+    #[arg(id = "root_folder", short, long, default_value = "./www")]
+    root_folder_path: String,
+    /// Number of threads that serve connections. Max 255.
+    #[arg(short, long, default_value_t = 4)]
+    threads_number: u8,
 }
