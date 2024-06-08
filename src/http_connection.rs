5142e0a4e7e16d14657179a2d6c30f172b2da51a
German <germangolozubow@gmail.com>
2024-06-08 12:31:12 +0300

    Code for reading the request is placed in a separate function

--- src/http_connection.rs
+++ src/http_connection.rs
@@ -1,4 +1,5 @@
 use std::io::{BufRead, Read};
+use std::net::TcpStream;
 use std::{io, net, time};
 use thiserror::Error;
 
@@ -28,56 +29,65 @@ impl HTTPConnection {
         let _ = stream.set_read_timeout(Some(time::Duration::from_millis(READ_TIMEOUT_MILLIS)));
         let mut buf_reader = io::BufReader::new(&stream);
 
-        // Need to find out if the request is an HTTP request.
-        // We are only interested in GET requests,
-        // so we need to make sure that the first 3 chars are "GET".
-        // "GET" in UTF-8 takes 3 bytes
-        let mut buf: [u8; 3] = [0; 3];
-        // Reading 3 bytes
-        let result = buf_reader.read_exact(&mut buf);
-        if let Err(error) = result {
-            return Err(Error::RequestReadError(error));
-        }
-        // Contains GET?
-        if buf != "GET".as_bytes() {
-            return Err(Error::WrongRequest);
-        }
+        let request = read_http_request(&mut buf_reader)?;
 
-        // This is a GET request.
-        // Try to read him
-
-        // Since there is a possibility that this request is formed incorrectly and has no end,
-        // we must limit the number of bytes to be read.
-        let mut request = String::with_capacity(MAX_REQUEST_READ_SIZE);
-        request.push_str("GET");
-        // Take guarantees that we will not be able to read more than MAX_REQUEST_READ_SIZE bytes,
-        // it will always return EOF
-        let mut take = buf_reader.take(MAX_REQUEST_READ_SIZE as u64);
-        loop {
-            // Read line from stream to the string
-            let result = take.read_line(&mut request);
-            match result {
-                Err(error) => return Err(Error::RequestReadError(error)),
-                // EOF reached, request is wrong or too large (> MAX_REQUEST_READ_SIZE)
-                Ok(0) => {
-                    println!("{}", request.len());
-                    return Err(Error::WrongRequest);
-                }
-                Ok(n) => {
-                    // Final line of the HTTP request is empty
-                    let last_line_is_empty = request.lines().last().unwrap().is_empty();
-                    if last_line_is_empty {
-                        break;
-                    }
-                }
-            }
-        }
         // Request has been read
         //println!("request:\n\"{request}\"");
         //println!("request length: {}", request.len());
 
         Ok(())
     }
+}
+
+/// Reads the HTTP request, returns Ok(String) if it is an HTTP request, otherwise it returns an error.
+fn read_http_request(buf_reader: &mut impl BufRead) -> Result<String, Error> {
+    // Need to find out if the request is an HTTP request.
+    // We are only interested in GET requests,
+    // so we need to make sure that the first 3 chars are "GET".
+    // "GET" in UTF-8 takes 3 bytes
+    let mut buf: [u8; 3] = [0; 3];
+    // Reading 3 bytes
+    let result = buf_reader.read_exact(&mut buf);
+    if let Err(error) = result {
+        return Err(Error::RequestReadError(error));
+    }
+
+    // Contains GET?
+    if buf != "GET".as_bytes() {
+        return Err(Error::WrongRequest);
+    }
+
+    // This is a GET request.
+    // Try to read him
+
+    // Since there is a possibility that this request is formed incorrectly and has no end,
+    // we must limit the number of bytes to be read.
+    let mut request = String::with_capacity(MAX_REQUEST_READ_SIZE);
+    request.push_str("GET");
+    // Take guarantees that we will not be able to read more than MAX_REQUEST_READ_SIZE bytes,
+    // it will always return EOF
+    let mut take = buf_reader.take(MAX_REQUEST_READ_SIZE as u64);
+    let mut line = String::new();
+    loop {
+        // Read line from stream to the string
+        let result = take.read_line(&mut line);
+        match result {
+            Err(error) => return Err(Error::RequestReadError(error)),
+            // EOF reached, request is wrong or too large (> MAX_REQUEST_READ_SIZE)
+            Ok(0) => {
+                return Err(Error::WrongRequest);
+            }
+            Ok(n) => {
+                // Final line of the HTTP request is empty
+                request += line.as_str();
+                if line == "\r\n" || line == "\n" {
+                    break;
+                }
+                line.clear();
+            }
+        }
+    }
+    Ok(request)
 }
 
 #[derive(Error, Debug)]
